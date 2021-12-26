a180f3713218f972653508833b5033d22dbbdf4d
Aydin Davutoglu <aydin.davutoglu@local.istatease.com>
2021-12-27 00:48:24 +0300

    added post card

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
 import Head from "next/head";
 import { PostCard, PostWidget, Categories } from "../components/index";
-const post = [
-  { title: "React Testing", excerpt: "Learn React Testing" },
-  { title: "React Web", excerpt: "Learn React web" },
-];
+import { getPosts } from "../services";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className="container mx-auto px-10 mb-8">
       <Head>
@@ -14,8 +11,8 @@ export default function Home() {
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
-          {post.map((post, index) => (
-            <PostCard post={post} key={post.title} />
+          {posts.map((post, index) => (
+            <PostCard post={post.node} key={post.title} />
           ))}
         </div>
         <div className="lg:col-span-4 col-span-1">
@@ -27,4 +24,11 @@ export default function Home() {
       </div>
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts },
+  };
 }
