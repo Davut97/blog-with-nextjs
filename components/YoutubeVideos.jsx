39fdc2303371e382678affdd0a6ee58e6bffb01f
LOCAL\aydin.davutoglu <aydindavutoglu@nmqdigital.com>
2022-02-18 14:28:03 +0300

    cotact form

--- components/YoutubeVideos.jsx
+++ components/YoutubeVideos.jsx
@@ -11,7 +11,7 @@ const YoutubeVideos = ({ videosLink }) => {
         title='YouTube video player'
         frameborder='0'
         allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-        allowfullscreen
+        allowFullScreen
       ></iframe>
     </div>
   );
