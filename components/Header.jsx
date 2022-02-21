cc340854843772c0a701ecab664c113498b2961f
LOCAL\aydin.davutoglu <aydindavutoglu@nmqdigital.com>
2022-02-21 21:08:51 +0300

    fixed typos

--- components/Header.jsx
+++ components/Header.jsx
@@ -4,31 +4,34 @@ const Header = () => {
   return (
     <div className='container mx-auto px-2 mb-8  sm:text-center'>
       <div className='w-full inline-block  py-8 text-[#E5A73E] text-center'>
-        <div className='md:float-left block text-center sm:float-right mb-4 '>
+        <div className='md:float-left  text-center  mb-4 lg:float-left sm:float-none '>
           <Link href='/'>
-            <span className='cursor-pointer font-bold text-5xl text-[#E5A73E] sm:text-center '>
+            <span className='cursor-pointer font-bold md:text-center text-5xl text-[#E5A73E] sm:text-center '>
               pisirenadam
             </span>
           </Link>
         </div>
 
-        <div className=' md:float-right md:contents sm:text-center md:pt-5'>
-          <Link href={`/contact`}>
-            <span className='md:float-right mt-2  ml-4 font-semibold  cursor-pointer'>
-              İletişim
+        <div className=' md:text-center  lg:float-right md:pt-5'>
+          <Link href={`/`}>
+            <span className='md:text-center place-self-start mt-2  ml-4 font-semibold  cursor-pointer'>
+              Anasayfa
+            </span>
+          </Link>
+          <Link href={`/blogs`}>
+            <span className='md:text-center sm:text-center mt-2 ml-4 font-semibold  cursor-pointer'>
+              Blog
             </span>
           </Link>
 
           <a href='https://mercure.nmq.digital/news/' target='_blank'>
-            <span className='md:float-right mt-2 ml-4 font-semibold  cursor-pointer'>Shop</span>
+            <span className='md:text-center sm:text-center mt-2 ml-4 font-semibold  cursor-pointer'>
+              Shop
+            </span>
           </a>
-
-          <Link href={`/blogs`}>
-            <span className='md:float-right mt-2 ml-4 font-semibold  cursor-pointer'>Blog</span>
-          </Link>
-          <Link href={`/`}>
-            <span className='md:float-right mt-2  ml-4 font-semibold  cursor-pointer'>
-              Anasayfa
+          <Link href={`/contact`}>
+            <span className='md:text-center sm:text-center mt-2  ml-4 font-semibold  cursor-pointer'>
+              İletişim
             </span>
           </Link>
         </div>
